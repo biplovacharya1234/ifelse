91f333c25b14d6d780af6e1bd4a98996c7e8be1d
Biplov <biplovacharya1234@gmail.com>
2024-05-28 12:06:41 -0500

    upto return argument

---
+++ function.py
@@ -0,0 +1,19 @@
+def greeting(name):
+    print(f'Hi {name}')
+
+
+greeting("Biplov")
+
+
+def fullname(firstname, lastname):
+    print(f'Hi {firstname} {lastname}')
+
+
+fullname("Biplov", "Acharya")
+
+
+def square(number):
+    return number*number
+
+
+print(square(9))
