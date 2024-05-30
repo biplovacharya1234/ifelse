6c8b0b1ace8f0257ae17f18e82197788d34a3c70
Biplov <biplovacharya1234@gmail.com>
2024-05-30 10:03:22 -0500

    class and object

---
+++ exceptions.py
@@ -0,0 +1,15 @@
+try:
+    age = int(input('Age:'))
+    income = 20000
+    risk = income/age
+    print(age)
+
+
+except ValueError:
+    print("Ivalid Value : Enter a valid Number")
+
+# hastag is used to write comments
+""" 
+this
+is for multiple line comment
+"""
