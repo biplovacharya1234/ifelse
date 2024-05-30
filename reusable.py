ee971f65aa90c43c6817d88ef3cb23a6131ba2f0
Biplov <biplovacharya1234@gmail.com>
2024-05-29 23:13:30 -0500

    upto return exceptions

---
+++ reusable.py
@@ -0,0 +1,14 @@
+def emoji_converter(message):
+    words = message.split(' ')
+    emoji = {
+        ":)": "😊",
+        ":(": "🙁"
+    }
+    output = ""
+    for word in words:
+        output += emoji.get(word, word) + " "
+    return output
+
+
+message = input(">")
+print(emoji_converter(message))
