46c9fd40e336c940c71536a81d1566048e7aac5a
Biplov <biplovacharya1234@gmail.com>
2024-05-31 20:39:09 -0500

    spreadsheet automation

---
+++ maxnumber.py
@@ -0,0 +1,4 @@
+from maxfunction import max_number
+
+numbers = [1, 2, 9, 4, 5]
+print(max_number(numbers))
---
+++ modules.py
@@ -0,0 +1,10 @@
+from converters import kg_to_lbs
+import converters
+
+print(converters.lbs_to_kg(167.5))
+
+# above method can be done in following ways as well
+# specific function from a module can be called
+
+
+print(kg_to_lbs(76))
